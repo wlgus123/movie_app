eed0ee8223312e12e41361a18dc5c2f4de6b5223
wlgus123 <mail30019794@gmail.com>
2024-02-12 11:19:12 +0900

    05-1 ~ 3 숫자 증감 기능 만들기, 생명주기 함수

--- src/App.js
+++ src/App.js
@@ -1,68 +1,51 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name} width={300} />
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('hello');
+  }
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    img:
-      "https://www.koreanbapsang.com/wp-content/uploads/2019/11/DSC_0831-e1645400234347.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    img:
-      "https://miro.medium.com/v2/resize:fit:1000/0*FJpFtXwW1RjmolYE.jpg",
-    rating: 5,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    img:
-      "https://upload.wikimedia.org/wikipedia/commons/4/44/Dolsot-bibimbap.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    img:
-      "https://www.marionskitchen.com/wp-content/uploads/2021/08/20201110_Spicy-Pork-Tonkatsu-5.jpg",
-    rating: 4.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    img:
-      "https://christieathome.com/wp-content/uploads/2020/07/spicy-tuna-kimbap-2.jpg",
-    rating: 5,
-  },
-];
+  state = {
+    count: 0,
+  };
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.img} rating={dish.rating} />
-      ))}
-    </div>
-  )
-}
+  add = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
+  componentDidMount() {
+    // 컴포넌트가 처음 화면에 그려지면 실행
+    console.log('component rendered');
+  }
+
+  componentDidUpdate() {
+    // 컴포넌트가 업데이트 될 때마다 출력
+    console.log('I just updated');
+  }
+
+  componentWillUnmount() {
+    // 컴포넌트가 죽을 때마다 출력
+    console.log('Goodbye, cruel world');
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
