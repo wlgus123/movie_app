2e4111e200842a77da3e3cf26b4afa9bf0fbc649
wlgus123 <mail30019794@gmail.com>
2024-02-13 18:51:50 +0900

    05-4 영화 앱 만들기 워밍업

--- src/App.js
+++ src/App.js
@@ -1,50 +1,19 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello');
-  }
-
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState((current) => ({
-      count: current.count + 1,
-    }));
-  };
-  minus = () => {
-    this.setState((current) => ({
-      count: current.count - 1,
-    }));
-  };
-
   componentDidMount() {
-    // 컴포넌트가 처음 화면에 그려지면 실행
-    console.log('component rendered');
+    // 영화 데이터 로딩!
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    // 컴포넌트가 업데이트 될 때마다 출력
-    console.log('I just updated');
-  }
-
-  componentWillUnmount() {
-    // 컴포넌트가 죽을 때마다 출력
-    console.log('Goodbye, cruel world');
-  }
-
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
   }
 }
 
