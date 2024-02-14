98465569c90d6daf7bddcb1a0157decc1b8397e9
wlgus123 <mail30019794@gmail.com>
2024-02-15 03:08:32 +0900

    06-2 영화 데이터 화면에 그리기

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ class App extends React.Component {
     movies: [],
   };
   getMovies = async () => {
-    const movies = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false });
   };
   componentDidMount() {
     this.getMovies();
